d43f541435d5839c2a6cca78cfc4c67a30481fcb
Leonardo Vinsen <leonardovinsen@gmail.com>
2021-05-10 18:14:08 +0700

    Add widgets for news category

---
+++ lib/widgets/screens/home_screen/news_screen/category_pills.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mongol/mongol.dart';
+
+class CategoryPill extends StatelessWidget {
+  final String text;
+
+  const CategoryPill({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+      height: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24.0),
+        color: Colors.lightBlue[200],
+      ),
+      child: MongolText(
+        text,
+        style: Theme.of(context).textTheme.headline2,
+      ),
+    );
+  }
+}
