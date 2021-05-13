fec177384c990cc484e1100dcbf3f8dc2c8c4c50
Leonardo Vinsen <leonardovinsen@gmail.com>
2021-05-13 15:23:44 +0700

    Delete HorizontalItems

--- lib/widgets/widget_index.dart
+++ lib/widgets/widget_index.dart
@@ -1,6 +1,5 @@
 // Home Screen
 export 'screens/home_screen/home_screen.dart';
-export 'screens/home_screen/books_screen/horizontal_items.dart';
 export 'screens/home_screen/books_screen/single_item.dart';
 
 // Detail Screen
